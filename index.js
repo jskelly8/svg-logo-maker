0daec899567139342a5954d2fc1ca227947538ad
Jess Skelton <jessskelton888@gmail.com>
2024-02-23 15:08:02 -0600

    Added inquirer questions & wrote function to generate and save SCG content on index.js

--- index.js
+++ index.js
@@ -6,13 +6,55 @@ const { Circle, Triangle, Square } = require('./lib/shapes');
 // Inquirer Questions
 const questions = () => {
     return inquirer.prompt([
-        {},
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter text for the logo (up to three characters):',
+            validate: input => input.length <= 3 ? true : 'Text must be up to three characters.'
+          },
+          {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter text color (color keyword or hexadecimal):',
+          },
+          {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['circle', 'triangle', 'square'],
+          },
+          {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter shape color (color keyword or hexadecimal):',
+          }
     ]);
 };
 
 // Function to generate SVG content and save to file
 const generateAndSave = () => {
+    let shapeInstance;
 
+    switch (shape) {
+        case 'circle':
+            shapeInstance = new Circle(shapeColor, textColor, text);
+            break;
+        case 'triangle':
+            shapeInstance = new Triangle(shapeColor, textColor, text);
+            break;
+        case 'square':
+            shapeInstance = new Square(shapeColor, textColor, text);
+            break;
+    }
+
+    const svgContent = shapeInstance.render();
+    fs.writeFile('./examples/logo.svg', svgContent, err => {
+        if (err) {
+        console.error('Error writing file:', err);
+        } else {
+        console.log('Generated logo.svg in the examples folder.');
+        }
+    });
 };
 
 // Main function to full run the application
